0d263a3d9433d200bd8f1bb3151bf33e18e9040c
dani0105 <danielrojas0105@gmail.com>
2020-06-10 22:00:30 -0600

    Init Commit

---
+++ src/main/java/com/dani/blacksmithmod/BlacksmithMod.java
@@ -0,0 +1,33 @@
+package com.dani.blacksmithmod;
+
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+
+@Mod("blacksmithmod")
+public class BlacksmithMod
+{
+
+    private static final Logger LOGGER = LogManager.getLogger();
+
+    public BlacksmithMod() {
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::doClientStuff);
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void setup(final FMLCommonSetupEvent event)
+    {
+
+    }
+
+    private void doClientStuff(final FMLClientSetupEvent event) {
+
+    }
+
+}
