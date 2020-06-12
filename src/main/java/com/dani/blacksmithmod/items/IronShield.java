271ba4cc4c1e071ea1ab3fa5977ef8c200f69101
dani0105 <danielrojas0105@gmail.com>
2020-06-12 12:10:05 -0600

    Iron Shield Item

---
+++ src/main/java/com/dani/blacksmithmod/items/IronShield.java
@@ -0,0 +1,11 @@
+package com.dani.blacksmithmod.items;
+
+import com.dani.blacksmithmod.BlacksmithMod;
+import net.minecraft.item.ShieldItem;
+
+public class IronShield extends ShieldItem {
+
+    public IronShield() {
+        super(new ShieldItem.Properties().group(BlacksmithMod.TAB));
+    }
+}
